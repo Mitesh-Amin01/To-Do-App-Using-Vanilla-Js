aab02e2f855f920ddeb7caf0d7474dafb415ce40
Mitesh-Amin01 <aminmiteshkumar.1484@gmail.com>
2025-06-07 17:58:44 +0530

    Remove UnWanted Variable!

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-import { completedTask, addForm, modal, addTaskBtn, closeModal, taskList, dynamicModalComponent } from './src/constant.js'
-import todosObj from './src/todo.model.js';
+import { addTaskBtn, dynamicModalComponent } from './src/constant.js';
 import dynamicModal from './src/modal.js';
-import customRenderTasks from './src/renderTask.component.js';
-import localStorageMethod from './src/localStorageMethod.js';
 import renderWraper from './src/renderWraper.js';
 
 addTaskBtn.addEventListener('click', () => {
--- src/constant.js
+++ src/constant.js
@@ -1,8 +1,5 @@
 const completedTask = document.getElementById('completedTask')
-const addForm = document.getElementById("taskForm");
-const modal = document.getElementById("taskModal");
 const addTaskBtn = document.getElementById("addTaskBtn");
-const closeModal = document.getElementById("closeModal");
 const taskList = document.getElementById("taskList");
 const dynamicModalComponent = document.getElementById("dynamicModelForAddAndEdit")
-export {completedTask, addForm, modal, addTaskBtn, closeModal, taskList, dynamicModalComponent}
+export {completedTask, addTaskBtn, taskList, dynamicModalComponent}
